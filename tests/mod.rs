5c562a4b1bb4358b060e5ea92d3e4624c9760a79
Oleg Ianchenko <olegian@cs.washington.edu>
2026-01-21 20:11:37 -0800

    add todo

--- tests/mod.rs
+++ tests/mod.rs
@@ -7,3 +7,5 @@ mod collections;
 // TODO: It's kind of annoying that `cargo test` ends up showing this
 // file in the output, but I honestly didn't like any of the quick solutions 
 // to this, and there are more interesting things to work on rn.
+
+// TODO: ADD MORE TESTS!
